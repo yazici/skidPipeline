4ccf81550a4e6873c9d857611e98f59a06d19ef8
Valentin <vdavid.pro@gmail.com>
2018-12-13 00:34:58 +0100

    added functions and UI to make light turn possible

--- lookdevUI.py
+++ lookdevUI.py
@@ -1,19 +1,19 @@
 import maya.cmds as cmds
 import os
 from functools import partial
-import commonTools
-import lookdevTools
 from pymel.core import *
 
 # **************** GLOBAL VARIABLES ****************
 
-lookdevWindow = "lookdevWindow"
 lookdevScenePath = os.path.abspath('//merlin/3d4/skid/09_dev/toolScripts/lookdev/lookdevSetup/LookdevSetup.ma')
 hdri_folder = os.path.abspath('//Merlin/3d4/skid/04_asset/SkidLibrary/HDRI')
 hdri_files = os.listdir(hdri_folder)
+hdriTurnGrp = '|LookdevSetup:GRP_LookdevSetup|LookdevSetup:GRP_LIGHTING|LookdevSetup:GRP_HDRI'
+turnLocator = 'turn_locator'
 grpName = 'LookdevSetup:GRP_LookdevSetup'
 lookdevPreset = 'SKID_LookdevSetup'
 scenePath = cmds.workspace(sn=True,q=True)
+turnDurations = [8,50,100]
 
 # **************** FUNCTIONS ****************
 
@@ -30,6 +30,7 @@ def CreateUI(*args):
 	rmanversion = cfg().rfm_env['versions']['rfm']
 	print rmanversion
 	if rmanversion != "22.1" :
+		import commonTools
 		commonTools.areeeeett()
 		cmds.evalDeferred("cmds.warning('Wrong Renderman for Maya version (installed version is %s), should be 22.1')" % rmanversion)
 	else :
@@ -40,13 +41,14 @@ def CreateUI(*args):
 		template.define(frameLayout, borderVisible=True, labelVisible=True)
 		template.define(text,h=25)
 		template.define(rowLayout)
+		template.define(checkBox,h=35)
 
 		try :
-			cmds.deleteUI(lookdevWindow)
+			cmds.deleteUI('lookdevWindow')
 		except RuntimeError :
 			pass
 
-		with window(lookdevWindow, title="Skid Lookdev Tools",menuBar=True,menuBarVisible=True) as win:
+		with window('lookdevWindow', title="Skid Lookdev Tools",menuBar=True,menuBarVisible=True) as win:
 			with template:
 				with frameLayout(l='Project'):
 					text('PROJECT IS SET TO')
@@ -55,11 +57,22 @@ def CreateUI(*args):
 				with frameLayout('Scene Setup'):
 					with columnLayout():
 						with rowLayout(numberOfColumns=3,ad3=1):
-							button(label='Import lookdev scene',w=198,c='import lookdevTools; reload(lookdevTools); lookdevTools.importLookdevScene()')
-							button(label='Hide',w=48,c='import lookdevTools; reload(lookdevTools); lookdevTools.hideLookdevScene()')
-							button(label='Reload',w=49,c='import lookdevTools; reload(lookdevTools); lookdevTools.reloadLookdevScene()')
-						button(l='Load Render Settings',c='import commonTools; reload(commonTools); commonTools.loadRenderSettings("%s")' %lookdevPreset)
-						# global chosenHDR
+							button(label='Import lookdev scene',w=198,\
+								c='import lookdevTools; \
+								reload(lookdevTools); \
+								lookdevTools.importLookdevScene()')
+							button(label='Hide',w=48, \
+								c='import lookdevTools; \
+								reload(lookdevTools); \
+								lookdevTools.hideLookdevScene()')
+							button(label='Reload',w=49, \
+								c='import lookdevTools; \
+								reload(lookdevTools); \
+								lookdevTools.reloadLookdevScene()')
+						button(l='Load Render Settings', \
+							c='import commonTools; \
+							reload(commonTools); \
+							commonTools.loadRenderSettings("%s")' %lookdevPreset)
 						optionMenu(changeCommand=chooseHDRfile)
 						for file in hdri_files:
 							menuItem(label=file)
@@ -68,21 +81,54 @@ def CreateUI(*args):
 					with columnLayout():
 						with rowLayout(numberOfColumns=2,ad2=1):
 							with columnLayout():
-								button(l='Create PxrSurface network',c='import lookdevTools; reload(lookdevTools); lookdevTools.createPxrSurfaceNetwork()')
-								button(l='Create PxrLayer network',c='import lookdevTools; reload(lookdevTools); lookdevTools.createPxrLayerNetwork()',en=False)
-								button(l='Convert PxrSurface to PxrLayer',c='import lookdevTools; reload(lookdevTools); lookdevTools.convertPxrSurfaceToLayer()',en=False)
+								button(l='Create PxrSurface network', \
+									c='import lookdevTools; \
+									reload(lookdevTools); \
+									lookdevTools.createPxrSurfaceNetwork()')
+								button(l='Create PxrLayer network', \
+									c='import lookdevTools; \
+									reload(lookdevTools); \
+									lookdevTools.createPxrLayerNetwork()',en=False)
+								button(l='Convert PxrSurface to PxrLayer', \
+									c='import lookdevTools; \
+									reload(lookdevTools); \
+									lookdevTools.convertPxrSurfaceToLayer()',en=False)
 
 				with frameLayout('Turn'):
 					with columnLayout():
 						with rowLayout(numberOfColumns=2,ad2=1):
-							button(label="Create turn locator",w=248,c='import lookdevTools; reload(lookdevTools); lookdevTools.create_turn_loc()')
-							button(label="Delete",  w=49, c='import lookdevTools; reload(lookdevTools); lookdevTools.delete_turn_loc()')
+							button(label="Create turn locator",w=248, \
+								c='import lookdevTools; \
+								reload(lookdevTools); \
+								lookdevTools.initTurn(%s)') % turnLocator
+							button(label="Delete",  w=49, \
+								c='import lookdevTools; \
+								reload(lookdevTools); \
+								lookdevTools.delete_turn_loc()')
 						with rowLayout(numberOfColumns=3,ad3=2):
-							button(label="%i frames" % 8, w=98,c='import lookdevTools; reload(lookdevTools); lookdevTools.change_frames_number(8)')
-							button(label="%i frames" % 50, w=98,c='import lookdevTools; reload(lookdevTools); lookdevTools.change_frames_number(50)')
-							button(label="%i frames" % 100, w=99,c='import lookdevTools; reload(lookdevTools); lookdevTools.change_frames_number(100)')
+							for i in turnDurations: # Create one button for each possible turn duration
+								button(label="%i frames" % i, w=98, \
+									c='import lookdevTools; \
+									reload(lookdevTools); \
+									lookdevTools.changeFrameRange(%s,%i)' % (turnLocator,i))
+						with rowLayout(numberOfColumns=3,ad3=2):
+							for i in turnDurations: # Create one button for each possible turn duration
+								button(label="%i frames" % i, w=98, \
+									c='import lookdevTools; \
+									reload(lookdevTools); \
+									lookdevTools.initTurn(%s) \
+									lookdevTools.changeFrameRange(%s,%i) \
+									lookdevTools.changeFrameRange(%s,%i,%i)' % (hdriTurnGrp,turnLocator,i,hdriTurnGrp,i,i))
+						with rowLayout(numberOfColumns=1):
+							button(label='Disable lighting turn' \
+								c='import lookdevTools; \
+								reload(lookdevTools) \
+								lookdevTools.disableTurn(%s)' % hdriTurnGrp)
 
 				with frameLayout('Nomenclatures'):
-					button(label='Afficher nomenclatures',h=30,c='import commonTools; reload(commonTools); commonTools.showNomenclatures()')
+					button(label='Afficher nomenclatures',h=30, \
+						c='import commonTools; \
+						reload(commonTools); \
+						commonTools.showNomenclatures()')
 
 CreateUI()
