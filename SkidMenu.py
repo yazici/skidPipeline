5d2c5fd1402d4dacff9674e1da1a71501835b52f
v.david <vdavid.pro@gmail.com>
2018-12-17 10:11:27 +0100

    Added CompactRenamer.py from Erik Lehmann

--- SkidMenu.py
+++ SkidMenu.py
@@ -6,10 +6,32 @@ def createMenu():
 	gMainWindow = mel.eval('$temp1 = $gMainWindow')
 	SkidToolsMenu = cmds.menu(parent = gMainWindow, label = "Skid Tools")
 	
-	cmds.menuItem(parent = SkidToolsMenu, label = "Increment and Save",c='import commonTools; reload(commonTools); commonTools.versionUp()')
-	cmds.menuItem(parent = SkidToolsMenu, label = "Animation Tools",c='import animationUI; reload(animationUI); animationUI.CreateUI()')
-	cmds.menuItem(parent = SkidToolsMenu, label = "Assets Tools",c='import assetsUI; reload(assetsUI); assetsUI.CreateUI()')
-	cmds.menuItem(parent = SkidToolsMenu, label = "Lookdev Tools",c='import lookdevUI; reload(lookdevUI); lookdevUI.CreateUI()')
-	cmds.menuItem(parent = SkidToolsMenu, label = "Previz Tools",c='import previzUI; reload(previzUI); previzUI.CreateUI()')
-	cmds.menuItem(parent = SkidToolsMenu, label = "Render Tools",c='import renderUI; reload(renderUI); renderUI.CreateUI()',en=False)
+	cmds.menuItem(parent = SkidToolsMenu, label = "Increment and Save", \
+		c='import commonTools; \
+		reload(commonTools); \
+		commonTools.versionUp()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Renamer", \
+		c='import CompactRenamer as ComRen; \
+		MCCR = ComRen.MainClassCompactRenamer(); \
+		MCCR.comRenUI()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Animation Tools", \
+		c='import animationUI; \
+		reload(animationUI); \
+		animationUI.CreateUI()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Assets Tools", \
+		c='import assetsUI; \
+		reload(assetsUI); \
+		assetsUI.CreateUI()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Lookdev Tools", \
+		c='import lookdevUI; \
+		reload(lookdevUI); \
+		lookdevUI.CreateUI()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Previz Tools", \
+		c='import previzUI; \
+		reload(previzUI); \
+		previzUI.CreateUI()')
+	cmds.menuItem(parent = SkidToolsMenu, label = "Render Tools", \
+		c='import renderUI; \
+		reload(renderUI); \
+		renderUI.CreateUI()',en=False)
 	cmds.menuItem(parent = SkidToolsMenu, label = "Rigging Tools",en=False)
